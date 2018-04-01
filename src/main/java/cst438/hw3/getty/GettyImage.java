d3ed352d00e7786a4213d815b6023c19141e772f
Donald Dong <xdong@csumb.edu>
2018-03-31 22:32:30 -0700

    added cross origin support

--- src/main/java/cst438/hw3/getty/GettyImage.java
+++ src/main/java/cst438/hw3/getty/GettyImage.java
@@ -6,15 +6,18 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URLEncoder;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/getty")
+@CrossOrigin(origins = {"https://twitter-getty-api.herokuapp.com"})
 public class GettyImage implements InitializingBean {
     @Value("${getty_images_key}")
     String getty_images_key;
@@ -24,12 +27,17 @@ public class GettyImage implements InitializingBean {
     }
 
     @RequestMapping("/search")
-    ResponseEntity<String> search(
+    @CrossOrigin(origins = {"https://twitter-getty-api.herokuapp.com"})
+    public ResponseEntity<String> search(
             @RequestParam(value="phrase") String phrase) throws Exception {
         RestTemplate rest = new RestTemplate();
-        return rest.exchange("https://api.gettyimages.com/v3/search/images?" +
+        ResponseEntity<String> response = rest.exchange("https://api.gettyimages.com/v3/search/images?" +
                 "embed_content_only=true&fields=comp&graphical_styles=photography&minimum_size=xx_large&" +
                 "sort_order=most_popular&phrase=" + URLEncoder.encode(phrase, "UTF-8"), HttpMethod.GET, entity, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccessControlAllowOrigin("https://twitter-getty-api.herokuapp.com");
+        headers.setAccessControlAllowHeaders(Arrays.asList("Origin", "X-Requested-With", "Content-Type", "Accept"));
+        return new ResponseEntity<>(response.getBody(), headers, response.getStatusCode());
     }
 
     @Override
--- src/main/java/cst438/hw3/twitter/Twitter.java
+++ src/main/java/cst438/hw3/twitter/Twitter.java
@@ -1,18 +1,18 @@
 package cst438.hw3.twitter;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URLEncoder;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/twitter")
+@CrossOrigin(origins = {"https://twitter-getty-api.herokuapp.com"})
 public class Twitter {
     private TwitterAuth auth;
     private HttpEntity<String> entity;
@@ -33,7 +33,16 @@ public class Twitter {
     public ResponseEntity<String> user_timeline(
             @RequestParam(value="screen_name") String screen_name) throws Exception {
         RestTemplate rest = new RestTemplate();
-        return rest.exchange("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" +
+        ResponseEntity<String> response = rest.exchange("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" +
                         URLEncoder.encode(screen_name, "UTF-8"), HttpMethod.GET, entity, String.class);
+        if (response.getStatusCode() != HttpStatus.ACCEPTED) {
+            entity = buildEntity(auth.getAccessToken());
+            response = rest.exchange("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" +
+                    URLEncoder.encode(screen_name, "UTF-8"), HttpMethod.GET, entity, String.class);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccessControlAllowOrigin("https://twitter-getty-api.herokuapp.com");
+        headers.setAccessControlAllowHeaders(Arrays.asList("Origin", "X-Requested-With", "Content-Type", "Accept"));
+        return new ResponseEntity<>(response.getBody(), headers, response.getStatusCode());
     }
 }
