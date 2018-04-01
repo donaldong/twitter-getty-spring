8fb6c623c9d2f29dd12cf8ce7104eb3ad95e91a6
Donald Dong <xdong@csumb.edu>
2018-03-31 21:34:23 -0700

    added getty api

--- src/main/java/cst438/hw3/application/Application.java
+++ src/main/java/cst438/hw3/application/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"cst438.hw3.application", "cst438.hw3.twitter"} )
+@ComponentScan(basePackages = {"cst438.hw3.application", "cst438.hw3.twitter", "cst438.hw3.getty"} )
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/cst438/hw3/getty/GettyImage.java
@@ -0,0 +1,41 @@
+package cst438.hw3.getty;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URLEncoder;
+
+@RestController
+@RequestMapping("/getty")
+public class GettyImage implements InitializingBean {
+    @Value("${getty_images_key}")
+    String getty_images_key;
+    HttpEntity<String> entity;
+
+    GettyImage() {
+    }
+
+    @RequestMapping("/search")
+    ResponseEntity<String> search(
+            @RequestParam(value="phrase") String phrase) throws Exception {
+        RestTemplate rest = new RestTemplate();
+        return rest.exchange("https://api.gettyimages.com/v3/search/images?" +
+                "embed_content_only=true&fields=comp&graphical_styles=photography&minimum_size=xx_large&" +
+                "sort_order=most_popular&phrase=" + URLEncoder.encode(phrase, "UTF-8"), HttpMethod.GET, entity, String.class);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Api-Key", getty_images_key);
+        entity = new HttpEntity<>(headers);
+    }
+}
